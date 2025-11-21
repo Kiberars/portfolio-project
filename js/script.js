ed07b5f9e4eba334576ce870fc58eaae3124e70e
kiberars <kiberars@gmail.com>
2025-11-21 15:56:29 +0500

    оделал бургер меню + стили и закрытие вне кона, также не залезает на бывше меню

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 const burger = document.querySelector('.burger');
-    const menu = document.querySelector('.nav-link')
+const menu = document.querySelector('.nav-link')
 
-    burger.addEventListener('click', () => {
+burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     menu.classList.toggle('active');
-    });
+});
+menu.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) => {
+    if (!menu.contains(e.target) && e.target !== burger) {
+        menu.classList.remove('active');
+        burger.classList.remove('active');
+    }
+});
